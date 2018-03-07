3fd30632836609e91d0adf011d5070523abc0133
Nimit Sohoni <nimitsohoni@group3.0ba1f3wsyyiernwly4ecs542ge.jx.internal.cloudapp.net>
2018-03-07 05:43:16 +0000

    Add option to use coverage and/or recombination as scalars to final layer; extract regression coefficients; avoid regularizing output layer

--- regression_final.py
+++ regression_final.py
@@ -25,7 +25,7 @@ del ambiguous_bases
 #random.seed(1)
 use_coverage = True
 use_recombination = True
-scalarize = False # When true, coverage and/or recombination are included as scalars instead
+scalarize = True # When true, coverage and/or recombination are included as scalars instead
 filter_indels = False
 forbidden_chroms = [1, 2]
 validation_chrom = 8#np.random.choice(20) + 3
@@ -39,9 +39,11 @@ num_train_ex = 600000
 epoch_val_frac = 0.1 # Fraction of examples to validate with after each epoch (out of total validation data)
 epochs = 5
 
+if not (use_coverage or use_recombination): scalarize = False
+
 num_indels = []
 seq = []
-if scalarize: scalar_data, scalar_data_val = [], []
+if scalarize: scalar_data = []
 for i in range(1, 24):
   if i in forbidden_chroms:
     continue
@@ -98,8 +100,7 @@ for i in range(1, 24):
       else:
         next_window = np.concatenate((next_window, recombination[window_lb:window_ub]), axis=1)
     seq_ch.append(next_window)
-    if scalarize:
-      scalar_data_ch.append(scalar_datum)
+    if scalarize: scalar_data_ch.append(scalar_datum)
   if i == validation_chrom:
     seq_val = seq_ch
     if scalarize: scalar_data_val = scalar_data_ch
@@ -115,22 +116,27 @@ del reference, referenceChr
 order = [x for x in range(len(seq))]
 random.shuffle(order)
 seq = np.asarray([seq[i] for i in order]) # Shuffle the training data, so we can easily choose a random subset for testing
+if scalarize: scalar_data = np.asarray([scalar_data[i] for i in order])
 num_indels = np.asarray([num_indels[i] for i in order])
 
 x_train = np.asarray(seq[:num_train_ex])
 y_train = np.asarray(num_indels[:num_train_ex])
 x_test = np.asarray(seq_val)
 y_test = np.asarray(num_indels_val)
+if scalarize:
+  s_train = scalar_data[:num_train_ex]
+  s_test = np.asarray(scalar_data_val)
 epoch_val_indices = np.random.choice(len(x_test), int(len(x_test) * epoch_val_frac), replace=False)
 
 print('Mean # indels per window: {}'.format(float(sum(y_train))/len(y_train)))
 
 import keras
 from keras.regularizers import l2
-from keras.layers import Conv1D, Dense, Flatten, Dropout
+from keras.layers import Conv1D, Dense, Dropout, Flatten, Lambda, Merge
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential
 from keras.callbacks import *
+import warnings
 
 use_lrelu = False
 activation = 'linear' if use_lrelu else 'relu' # make linear to enable advanced activations
@@ -139,37 +145,68 @@ reg = l2(reg_param)
 
 model = Sequential()
 # First convolutional layer. Input channels: [A,C,G,T] and coverage or recomb if applicable
-model.add(Conv1D(40, kernel_size=5, activation=activation, input_shape=(expanded_window_size, 4 + use_coverage + use_recombination), kernel_regularizer=reg))
+model.add(Conv1D(40, kernel_size=5, activation=activation, input_shape=(expanded_window_size, 4 + (not scalarize)*(use_coverage + use_recombination)), kernel_regularizer=reg))
 if use_lrelu: model.add(LeakyReLU(alpha=0.1))
 model.add(Conv1D(100, kernel_size=8, activation=activation, kernel_regularizer=reg))
 if use_lrelu: model.add(LeakyReLU(alpha=0.1))
 model.add(Flatten())
 model.add(Dense(1024, activation=activation, kernel_regularizer=reg)) # FC hidden layer 1
 if use_lrelu: model.add(LeakyReLU(alpha=0.1))
-model.add(Dense(1, activation='relu', kernel_regularizer=reg)) # Output layer. ReLU activation because we are trying to predict a nonnegative value!
+if scalarize: # Add the scalar data in right before the output layer
+  scalar_branch = Sequential()
+  Identity = Lambda(lambda x: x + 0, input_shape=(use_coverage + use_recombination,))
+  scalar_branch.add(Identity) # Hack to avoid doing any transformation on the scalar data
+  def Concatenate(x):
+    with warnings.catch_warnings(): # keras.layers.Merge is deprecated
+      warnings.simplefilter('ignore')
+      return Merge(x, mode='concat')
+  merged = Concatenate([model, scalar_branch])
+  final_model = Sequential()
+  final_model.add(merged)
+else:
+  final_model = model
+final_model.add(Dense(1, activation='relu')) # Output layer. ReLU activation because we are trying to predict a nonnegative value!
 
-def rms(y_true, y_pred): return keras.backend.sqrt(keras.losses.mean_squared_error(y_true, y_pred))
+def rms(y_true, y_pred): return keras.backend.sqrt(keras.losses.mean_squared_error(y_true, y_pred)) # TODO: For some reason rms is totally different than sqrt(mse)... why??
 
-model.compile(loss=keras.losses.mean_squared_error,
-              optimizer=keras.optimizers.Adam(),
-              metrics=['mse', 'mae']) # Minimize the MSE. Report mean absolute error and mean squared error. TODO: For some reason rms is totally different than sqrt(mse)... why??
+final_model.compile(loss=keras.losses.mean_squared_error,
+                    optimizer=keras.optimizers.Adam(),
+                    metrics=['mse', 'mae']) # Minimize the MSE. Report mean absolute error and mean squared error.
 
 setup_time = time.time()
 print('Total setup time: {} sec'.format(setup_time - start_time))
 
 logfile = 'KerasTrainingLog.csv'
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test[epoch_val_indices], y_test[epoch_val_indices]),
-          callbacks=[CSVLogger(logfile)])
+if scalarize:
+  train_input = [x_train, s_train]
+  test_input = [x_test, s_test]
+  val_input = ([x_test[epoch_val_indices], s_test[epoch_val_indices]], y_test[epoch_val_indices])
+else:
+  train_input = x_train
+  test_input = x_test # This is really validation also, but we aren't "testing" yet
+  val_input = (x_test[epoch_val_indices], y_test[epoch_val_indices])
+final_model.fit(train_input, y_train,
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=1,
+                validation_data=val_input,
+                callbacks=[CSVLogger(logfile)])
 
 train_time = time.time()
 print('Total training time: {} sec'.format(train_time - setup_time))
 
+if scalarize:
+  output_weights = np.squeeze(final_model.get_layer(index=-1).get_weights()[0]) # get_weights returns [weights, biases]
+  # Print linear regression coefficients
+  ind = -1
+  if use_recombination:
+    print('Recombination coefficient: {}'.format(output_weights[ind]))
+    ind -= 1
+  if use_coverage:
+    print('Coverage coefficient: {}'.format(output_weights[ind]))
+
 # Predictions on the test set
-y_pred = utils.flatten(model.predict(x_test, batch_size=batch_size, verbose=1))
+y_pred = utils.flatten(final_model.predict(test_input, batch_size=batch_size, verbose=1))
 np.save(data_dir + 'RegrKerasWindowPred.npy', y_test)
 np.save(data_dir + 'RegrKerasWindowPred.npy', y_pred)
 
